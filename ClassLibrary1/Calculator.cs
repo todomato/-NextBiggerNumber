366680a0431eb0c68559ad80e17b0cbe48fd4eaa
todomato <dreamrrrr@hotmail.com>
2017-03-13 18:04:41 +0800

    init

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public object number(int num)
+        {
+            var intAry = num.ToString().ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToList();
+
+            if (intAry.Count == 1)
+            {
+                return -1;
+            }
+
+            if (intAry[1] > intAry[0])
+            {
+                var temp = intAry[0];
+                intAry[0] = intAry[1];
+                intAry[1] = temp;
+            }
+           
+
+            int result = GetNewNumber(intAry);
+            return result == num ? -1 : result;
+        }
+
+        private int GetNewNumber(List<int> intAry)
+        {
+            var count = intAry.Count;
+
+            var result = intAry.Select((x, index) => x * (Math.Pow(10, count - index - 1))).Sum();
+
+            return Convert.ToInt32(result);
+        }
+    }
+}
---
+++ NextBiggerNumber/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace NextBiggerNumber
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void give_9_return_negative1()
+        {
+            var target = new Calculator();
+            var actual = target.number(9);
+
+            var expect = -1;
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void give_12_return_21()
+        {
+            var target = new Calculator();
+            var actual = target.number(12);
+
+            var expect = 21;
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void give_21_return_21()
+        {
+            var target = new Calculator();
+            var actual = target.number(21);
+
+            var expect = -1;
+
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
