db0bb6a2fa81238d4ffa169a7c6fb3dd21f69277
todomato <dreamrrrr@hotmail.com>
2017-03-14 18:00:12 +0800

    576 -> 657 & refactoring

--- ClassLibrary1/Calculator.cs
+++ ClassLibrary1/Calculator.cs
@@ -6,24 +6,39 @@ namespace ClassLibrary1
 {
     public class Calculator
     {
-        public object number(int num)
+        public object Next(int num)
         {
             var intAry = num.ToString().ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToList();
 
             //for loop 消除 magic number
             for (int index = intAry.Count - 1; index > 0; index--)
             {
-                if (intAry[index] > intAry[index-1])
+                if (intAry[index] > intAry[index - 1])
                 {
-                    var temp = intAry[index - 1];
-                    intAry[index - 1] = intAry[index];
-                    intAry[index] = temp;
+                    var r = intAry.GetRange(index, intAry.Count - index);  //.Skip(index).Take(intAry.Count - index).ToList();
+                    var l = intAry.GetRange(0, index); //.Take(index).ToList(); //包含t
 
-                    return GetNewNumber(intAry);
+                    Swap(intAry, index, r, l);
+
+                    l.AddRange(r.OrderBy(x => x));
+                    return GetNewNumber(l);
                 }
             }
             
             return  -1;
+        }
+
+        private static void Swap(List<int> intAry, int index, List<int> r, List<int> l)
+        {
+            for (int i = r.Count - 1; i >= 0; i--)
+            {
+                if (r[i] > intAry[index - 1])
+                {
+                    l[index - 1] = r[i];
+                    r[i] = intAry[index - 1];
+                    break;  //找到第一個swap的, 就是比t大的最小值
+                }
+            }
         }
 
         private int GetNewNumber(List<int> intAry)
--- NextBiggerNumber/UnitTest1.cs
+++ NextBiggerNumber/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace NextBiggerNumber
         public void give_9_return_negative1()
         {
             var target = new Calculator();
-            var actual = target.number(9);
+            var actual = target.Next(9);
 
             var expect = -1;
 
@@ -22,7 +22,7 @@ namespace NextBiggerNumber
         public void give_12_return_21()
         {
             var target = new Calculator();
-            var actual = target.number(12);
+            var actual = target.Next(12);
 
             var expect = 21;
 
@@ -33,11 +33,33 @@ namespace NextBiggerNumber
         public void give_21_return_21()
         {
             var target = new Calculator();
-            var actual = target.number(21);
+            var actual = target.Next(21);
 
             var expect = -1;
 
             Assert.AreEqual(expect, actual);
         }
+
+        
+        [TestMethod]
+        public void give_345_return_354()
+        {
+            var target = new Calculator();
+            var actual = target.Next(345);
+
+            var expect = 354;
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void input_is_576_should_return_657()
+        {
+            var target = new Calculator();
+            var expected = 657;
+            var actual = target.Next(576);
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
